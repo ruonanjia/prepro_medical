9b4eb8908116c15f788ff3522304ab435540a021
ruonanjia <ruonjia@gmail.com>
2019-05-24 16:34:15 -0400

    Change path

--- fmriPrepSingularity.sh
+++ fmriPrepSingularity.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=general
-#SBATCH --job-name=fp2
+#SBATCH --job-name=fpm1
 #SBATCH --ntasks=1 --nodes=1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem-per-cpu=8000
@@ -12,14 +12,14 @@
 echo "Running script"
 
 singularity run --cleanenv /project/ysm/levy_ifat/fmriPrep/fmriprep-latest.simg \
-/home/rj299/scratch60/ra_ptsd/fmriprep_input /home/rj299/scratch60/ra_ptsd/fmriprep_output \
+/home/rj299/scratch60/ra_medical/fmriprep_input /home/rj299/scratch60/ra_medical/fmriprep_output \
 participant \
 --skip_bids_validation \
 --fs-license-file /home/rj299/freesurfer_license/licenseFreeSurfer.txt \
 -w /home/rj299/scratch60/work \
 --nthreads 12 \
 --ignore slicetiming \
---participant_label { 43 56 }
+--participant_label { 2661 }
 
 
 
